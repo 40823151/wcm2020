f4ba43605ed986fc8b7110b249afd9eceae5d850
40823151 <40823151@gm.nfu.edu.tw>
2020-06-25 21:40:06 +0800

    blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40823151'
+SITENAME = '期末作業'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
